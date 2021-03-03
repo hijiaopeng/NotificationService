13460ab3a689f6e51f0ad37add838184812f7a93
hijiaopeng <hijiaopeng@outlook.com>
2021-03-03 20:02:10 +0800

    Initial commit

---
+++ app/src/main/java/com/jiaopeng/notificationservice/MyService.kt
@@ -0,0 +1,52 @@
+package com.jiaopeng.notificationservice
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+
+/**
+ * 描述：
+ *
+ * @author JiaoPeng by 3/3/21
+ */
+class MyService : Service() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "my_service",
+                "前台服务通知",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            manager.createNotificationChannel(channel)
+        }
+        val intent = Intent(this, MainActivity::class.java)
+        val pi = PendingIntent.getActivity(this, 0, intent, 0)
+        val notification = NotificationCompat.Builder(this, "my_service")
+            .setContentTitle("这是标题")
+            .setContentText("这是内容")
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+            .setContentIntent(pi)
+            .build()
+        startForeground(1, notification)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+}
